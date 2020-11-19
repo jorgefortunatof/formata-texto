3ae3d0096dd59428544a676d79af0fe25542850e
jorgefortunatof <jorgefortunatof@gmail.com>
2020-11-18 21:04:29 -0300

    Added eslint and prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,11 @@
 import type { AppProps } from 'next/app';
 import GlobalStyle from '../styles/global';
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-	return (
-		<>
-			<Component {...pageProps} />
-			<GlobalStyle />
-		</>
-	);
-};
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
+	<>
+		<Component {...pageProps} />
+		<GlobalStyle />
+	</>
+);
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,15 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
-const Home: React.FC = () => {
-	return (
-		<div>
-			<Head>
-				<title>Create Next App</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<main>
-				<h1>FormataTexto</h1>
-			</main>
-		</div>
-	)
-}
+const Home: React.FC = () => (
+	<div>
+		<Head>
+			<title>Create Next App</title>
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+		<main>
+			<h1>FormataTexto</h1>
+		</main>
+	</div>
+);
 
 export default Home;
